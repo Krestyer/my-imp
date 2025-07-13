258386be78517fe71406b65f704294e029222c26
Krestyer <137534839+Krestyer@users.noreply.github.com>
2025-07-13 09:53:24 +0000

    Add --hello argument to say hello

--- node.py
+++ node.py
@@ -1,7 +1,10 @@
 import argparse
 
 def main():
+        if args.hello:
+            print(f"Hello, {args.hello}!")
     parser = argparse.ArgumentParser(description='Simple CLI')
+    parser.add_argument("--hello", help="Say hello")
     args = parser.parse_args()
 
 if __name__ == '__main__':
